6bed258b5e8c5499840f24b0504df7efa077745c
Lucas Bragança <lucas.braganca@thoughtworks.com>
2021-01-12 14:42:24 -0300

    Changes mutex to a RW

--- server/event/store.go
+++ server/event/store.go
@@ -2,44 +2,39 @@ package event
 
 import (
 	"sync"
+	"sync/atomic"
 )
 
 // Store stores observers and notify them when a new event is dispatched
 type Store struct {
-	subLock   *sync.Mutex
-	id        int
-	observers map[int]chan<- string
+	observerLock *sync.RWMutex
+	id           uint64
+	observers    map[uint64]chan<- string
 }
 
 // NewStore returns a new instance of a Store
 func NewStore() *Store {
 	return &Store{
-		subLock:   &sync.Mutex{},
-		observers: make(map[int]chan<- string),
+		observerLock: &sync.RWMutex{},
+		observers:    make(map[uint64]chan<- string),
 	}
 }
 
 // Subscribe adds a new observer to the store through a channel to be notified.
 // It returns a function that when called unsubscribes the observer from the store.
 func (e *Store) Subscribe(channel chan<- string) func() {
-	e.subLock.Lock()
-	defer e.subLock.Unlock()
+	id := e.nextID()
 
-	e.observers[e.id] = channel
+	e.addObserver(id, channel)
 
-	result := func(m map[int]chan<- string, id int) func() {
-		return func() {
-			delete(m, id)
-		}
-	}(e.observers, e.id)
-
-	e.id++
-
-	return result
+	return func() { e.removeObserver(id) }
 }
 
 // Dispatch sends a new event to the store, broadcasting to all observers
 func (e *Store) Dispatch(message string) {
+	e.observerLock.RLock()
+	defer e.observerLock.RUnlock()
+
 	for _, c := range e.observers {
 		select {
 		case c <- message:
@@ -51,5 +46,30 @@ func (e *Store) Dispatch(message string) {
 
 // Subscriptions return the number of observers subscribed to the store
 func (e *Store) Subscriptions() int {
+	return e.countObservers()
+}
+
+func (e *Store) nextID() uint64 {
+	return atomic.AddUint64(&e.id, 1)
+}
+
+func (e *Store) addObserver(id uint64, channel chan<- string) {
+	e.observerLock.Lock()
+	defer e.observerLock.Unlock()
+
+	e.observers[id] = channel
+}
+
+func (e *Store) removeObserver(id uint64) {
+	e.observerLock.Lock()
+	defer e.observerLock.Unlock()
+
+	delete(e.observers, id)
+}
+
+func (e *Store) countObservers() int {
+	e.observerLock.RLock()
+	defer e.observerLock.RUnlock()
+
 	return len(e.observers)
 }
